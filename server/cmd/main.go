a9af14450841352b2718afcb41ef8edacba45837
salernoelia <putopet@gmail.com>
2024-05-21 10:29:44 +0200

    rm main -env

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,15 +4,9 @@ import (
 	"log"
 	"net/http"
 	"server/pkg/routers"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	 err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
 
     router := routers.NewRouter()
     log.Println("Server is now accessible on the local network on port 8080")
