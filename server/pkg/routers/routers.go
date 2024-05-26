d64176496e7fe8a6df5e64050c8145fd42469cb0
salernoelia <putopet@gmail.com>
2024-05-26 10:00:16 +0200

    rm: testLLM

--- server/pkg/routers/routers.go
+++ server/pkg/routers/routers.go
@@ -13,7 +13,6 @@ func NewRouter() *mux.Router {
     router.HandleFunc("/meteoblue", handlers.FetchMeteoBlue).Methods("GET")
     router.HandleFunc("/hotareas", handlers.FetchAndReportHotAreas).Methods("GET")
     router.HandleFunc("/hotareasgps", handlers.FetchAndReportHotAreasBasedOnLocation).Methods("POST")
-    router.HandleFunc("/llmtest", handlers.TestLLM).Methods("GET")
     router.HandleFunc("/speak", handlers.TTSTest).Methods("POST")
     router.HandleFunc("/weather", handlers.FetchAndSpeakWeatherData).Methods("GET")
     router.HandleFunc("/weathergps", handlers.FetchAndSpeakWeatherBasedOnGPS).Methods("POST")
